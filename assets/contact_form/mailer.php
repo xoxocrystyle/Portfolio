3e3dd4240a0aa08368e183336f906ce8bacbb51a
Crystal Shir Navarro <crystal.shir@gmail.com>
2018-04-12 16:31:24 -0700

    Configured PHP on my server

--- assets/contact_form/mailer.php
+++ assets/contact_form/mailer.php
@@ -2,7 +2,7 @@
 require_once('email_config.php');
 require('phpmailer/PHPMailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
-$mail->SMTPDebug = 3;           // Enable verbose debug output. Change to 0 to disable debugging output.
+$mail->SMTPDebug = 0;           // Enable verbose debug output. Change to 0 to disable debugging output.
 
 $mail->isSMTP();                // Set mailer to use SMTP.
 $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers.
