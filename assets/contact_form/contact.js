0e6b187c8afd07e2153723cdab00d81f1946b7cf
Crystal Shir Navarro <crystal.shir@gmail.com>
2018-05-25 16:46:36 -0700

    Minor changes for approval

--- assets/contact_form/contact.js
+++ assets/contact_form/contact.js
@@ -45,22 +45,3 @@ $('.xone-contact button[name=submit]').click(function() {
 		return false;
 	});
 
-
-// $(document).ready(function(){
-
-
-//     var from,to,subject,text;
-//     $("#send_email").submit(function(){      
-//         to=$("#to").val();
-//         subject=$("#subject").val();
-//         text=$("#content").val();
-//         $("#message").text("Sending E-mail...Please wait");
-//         $.get("http://localhost:3000/send",{to:to,subject:subject,text:text},function(data){
-//         if(data=="sent")
-//         {
-//             $("#message").empty().html("Email is been sent. Thankyou!");
-//         }
-
-// });
-//     });
-// });
